37e399cf1351fc7e73bf7340b47ed6b1f8f1288a
NexusDevelopmentUA <unnexusgcm@gmail.com>
2019-03-05 06:34:54 +0200

    Add project files.

---
+++ WebPageParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebPageParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
